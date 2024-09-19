4ed1f0c163c3314aec8a0cbb2dbc18c5c0a7cde1
isabelzambrana <zambranachavezisabel@pedropoveda.edu.bo>
2024-09-19 09:12:11 -0400

    Add files via upload

--- Hukkallpallacalendario.php
+++ Hukkallpallacalendario.php
@@ -5,9 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Actividades y Proyectos</title>
-  <link rel="Stylesheet"href=".Hukkallpallacalendario.css">
+  <link rel="Stylesheet"href="Hukkallpallacalendario.css">
 </head>
-<body>
+<body id="principal">
   <?php
   require 'db.php';
   require '1 Encabezado.php';
@@ -18,33 +18,33 @@
     </section>
 
     <center><h1>Proyectos</h1></center><!--Proyectos-->
-<div class="contenedor">
+<section class="proyectos">
 
    <!--ecocanje-->
-   <div class="caja" id="ecocanje">
+   <div class="ecocanje" id="ecocanje">
         <h2>ecocanje</h2>
-        <img   src=ecocanje.png  >
+        <img class="uno" src="ecocanje.png"  >
     </div>  
     
      
   
     <!--Compostar--> 
-    <div class="caja" id="compostar">
+    <div class="compostar" id="compostar">
         <h2>Compostar</h2>
-        <img src=compost.jpg  width="230" height="230">
+        <img class="dos"src="compost.jpg"  width="230" height="230" >
     </div>   
       
      
 
 
    <!--huertos sanos-->          
-   <div class="caja" id="huertos_sanos">
+   <div class="huertos" id="huertos_sanos">
         <h2>Huertos sanos</h2>
-        <img src=huerto.jpg  width="230" height="230">
+        <img class="tres" src="huerto.jpg"  width="230" height="230" >
    </div>
 
 
-</div>
+</section>
 </body>
 <?php
 require '7 pie de pagina.php';
